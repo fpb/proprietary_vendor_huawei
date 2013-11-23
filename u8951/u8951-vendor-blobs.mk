97a242f68101d336bf3890db413ecfc5504d3239
Fernando Birra <fernando.birra@gmail.com>
2013-11-23 20:21:03 +0000

    NFC support

--- u8951/u8951-vendor-blobs.mk
+++ u8951/u8951-vendor-blobs.mk
@@ -215,3 +215,9 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/u8951/proprietary/wifi/ath6k/nullTestFlow.bin:system/wifi/ath6k/nullTestFlow.bin \
     vendor/huawei/u8951/proprietary/wifi/ath6k/softmac:system/wifi/ath6k/softmac \
     vendor/huawei/u8951/proprietary/wifi/ath6k/utf.bin:system/wifi/ath6k/utf.bin 
+# NFC Related
+PRODUCT_COPY_FILES += \
+    vendor/huawei/u8951/proprietary/bin/nfc_check:system/bin/nfc_check \
+    vendor/huawei/u8951/proprietary/lib/libpn544_fw.so:system/lib/libpn544_fw.so \
+    vendor/huawei/u8951/proprietary/lib/hw/nfc.huawei.so:system/lib/hw/nfc.huawei.so
+
